8b612d9410f686cd3a769c22c6c15880623d7170
Peter Benzoni <pbenzoni@gmfus.org>
2025-09-04 17:43:29 -0400

    adding port

--- app.py
+++ app.py
@@ -68,5 +68,6 @@ def index():
     ]
     return render_template('index.html', tools=tools)
 
-if __name__ == '__main__':
-        app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render sets $PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
